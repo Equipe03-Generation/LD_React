34aaa01a77f4c73ee060a6dc0c91bfa159bdab96
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-11-30 11:55:41 -0300

    Add files via upload

--- src/paginas/equipe/Equipe.tsx
+++ src/paginas/equipe/Equipe.tsx
@@ -1,10 +1,30 @@
 import { Grid } from '@material-ui/core';
+import { Box } from '@mui/material';
 import React from 'react';
 
 function Equipe() {
     return (
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <p>Teste</p>
+        <Grid justifyContent='center' alignItems='center'>
+            <Box textAlign='center'>
+            <h1>Equipe Desenvolvedora</h1>
+            </Box>
+            <Box textAlign='center'>
+            <h3>
+                - Ana Carolina 
+                <br></br>
+                - Raquel Soares
+                <br></br>
+                - Lucas Campos 
+                <br></br>
+                - Gustavo Rodrigues 
+                <br></br>
+                - Kaio Lima
+                <br></br>
+                - Jose Luiz
+                <br></br>
+            </h3>
+            </Box>
+            
         </Grid>
     )
 }
--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -5,9 +5,13 @@ import { Box } from '@mui/material';
 
 function Sobre() {
     return (
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
+        <Grid justifyContent='center' alignItems='center'>
+            <Box textAlign='center'>
             <h1>Sobre o projeto Dev Girls</h1>
-            <p>Fórum de interações que abrange a conversação entre mulheres que são da área e mulheres interessadas a ingressar, com foco em perguntas e respostas sobre inovações e conceitos de programação. Visto que há uma desigualdade, esse fórum tem um público alvo pois trata-se de uma ação afirmativa com o intuito de colocar mais mulheres no ramo da tecnologia.</p>
+            </Box>
+            <Box textAlign='center'>
+            <h3>Fórum de interações que abrange a conversação entre mulheres que são da área e mulheres interessadas a ingressar, com foco em perguntas e respostas sobre inovações e conceitos de programação. Visto que há uma desigualdade, esse fórum tem um público alvo pois trata-se de uma ação afirmativa com o intuito de colocar mais mulheres no ramo da tecnologia.</h3>
+            </Box>
         </Grid>
     )
 }
