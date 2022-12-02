436c77c30b9007e01fdfebae9a6ba9169f259c2f
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-12-02 10:31:35 -0300

    Atualização contendo login e cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,18 @@ import Sobre from './paginas/sobre/Sobre';
 import Home from './paginas/home/Home';
 import Equipe from './paginas/equipe/Equipe';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/cadastrousuario' element={<CadastroUsuario />} />
         <Route path='/sobre' element={<Sobre />} />
         <Route path='/equipe' element={<Equipe />} />
       </Routes>
