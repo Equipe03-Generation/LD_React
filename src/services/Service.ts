936f1b213f8e236b5936d7912b29aafedf3fd21a
Lady Debug <117478012+Equipe03-Generation@users.noreply.github.com>
2022-12-02 21:42:57 -0300

    versão de testes (login)

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,7 +4,7 @@ export const api = axios.create({
     baseURL: 'https://forum-dev-girls.onrender.com'
 })
 
-export const cadastroUsuario = async(url:any, dados:any, setDado:any) => {
+export const cadastrarUsuario = async(url:any, dados:any, setDado:any) => {
     const resposta = await api.post(url, dados)
         setDado(resposta.data)
 }
