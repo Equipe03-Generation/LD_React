a5e35985790a70099ace89f8c427150f363b2e6d
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-12-05 11:08:45 -0300

    Atualização do Navbar com Logout

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
 import './Navbar.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
 
 function Navbar() {
+
+    const [token, setToken] = useLocalStorage('token')
+    let navigate = useNavigate()
+
+    function goLogout(){
+        setToken('')
+        alert("Conta desconectada com sucesso!")
+        navigate('/login')
+    }
+
     return (
         <>
             <AppBar position="static">
@@ -36,6 +47,32 @@ function Navbar() {
                                 Equipe
                             </Link>
                             </Typography>
+                            <Box mx={1} className='Cursor'>
+                            <Typography variant="h6" color="inherit">
+                            <Link to='/' className='textobranco'>
+                                Postagens
+                                </Link>
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='Cursor'>
+                            <Typography variant="h6" color="inherit">
+                            <Link to='/' className='textobranco'>
+                                Temas
+                                </Link>
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='Cursor'>
+                            <Typography variant="h6" color="inherit">
+                            <Link to='/' className='textobranco'>
+                                Cadastrar Tema
+                                </Link>
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='Cursor' onClick={goLogout}>
+                            <Typography variant="h6" color="inherit">
+                                Sair
+                            </Typography>
+                        </Box>
                         </Box>
                     </Box>
 
