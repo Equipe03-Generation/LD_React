c4ab2810da7e46703a41817eb07eca88dfe600af
Lady Debug <117478012+Equipe03-Generation@users.noreply.github.com>
2022-12-09 11:00:13 -0300

    Add files via upload

---
+++ Task_09_08.12/README.md
@@ -0,0 +1,3 @@
+Branch da v2
+<br>
+Versão esboço utilizada para a implantação de tema, postagem, modal e store.
---
+++ Task_09_08.12/src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import GithubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Typography, Grid } from '@material-ui/core';
+import { Box } from '@mui/material';
+import './Footer.css'
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { useSelector } from 'react-redux';
+
+function Footer() {
+        const token = useSelector<TokenState, TokenState["tokens"]>(
+            (state) => state.tokens
+          );
+          var footerComponent;
+          if(token != ""){
+            footerComponent = <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid alignItems="center" item xs={12}>
+                <Box className='box1'>
+                    <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                        <Typography variant="h5" align="center" gutterBottom className='textos'>Sigam-nos nas redes sociais </Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" justifyContent="center">
+                        <a href="https://github.com/Equipe03-Generation" target="_blank">
+                            <GithubIcon className='redes' />
+                        </a>
+                        <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                            <InstagramIcon className='redes' />
+                        </a>
+                        <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                            <LinkedInIcon className='redes' />
+                        </a>
+                    </Box>
+                </Box>
+                <Box className='box2'>
+                    <Box paddingTop={1}>
+                        <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2022 Copyright:</Typography>
+                    </Box>
+                    <Box>
+                        <a target="_blank" href="https://github.com/Equipe03-Generation">
+                            <Typography variant="subtitle2" gutterBottom className='textos' align="center">github.com/Equipe03-Generation</Typography>
+                        </a>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+          }
+
+    return (
+        <>
+        {footerComponent}
+        </>
+    );
+}
+
+export default Footer;
